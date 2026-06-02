e746f8f3ce6f039283dfc274f7a5192f887dfafa
aaaa90012008 <aaaa90012008@gmail.com>
2026-06-02 15:10:44 +0700

    fullstack FE + BE initial

---
+++ FE/src/container/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Footer(props) {
+  return (
+    <div>
+      <footer className="footer-area section_gap">
+      </footer>
+
+    </div>
+  );
+}
+
+export default Footer;
